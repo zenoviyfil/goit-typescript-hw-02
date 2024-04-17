beb0cc73ce026941c142d4a059613aab5df41690
Zenoviy Fil <98259328+zenoviyfil@users.noreply.github.com>
2024-04-17 22:05:57 +0200

    try edit

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -1,6 +1,10 @@
 import css from "./ErrorMessage.module.css";
 
-const ErrorMessage = ({ message }) => {
+type ErrorMessage = {
+  message: string
+}
+
+const ErrorMessage = ({ message }:ErrorMessage) => {
   return (
   <p className={css.errorMessage}>{message}</p>
   );
--- src/components/ImageCard/ImageCard.tsx
+++ src/components/ImageCard/ImageCard.tsx
@@ -1,8 +1,23 @@
 import css from "./ImageCard.module.css";
+
+type Image = {
+  urls: {
+    regular: string
+    small: string
+  }
+  likes: number
+  description: string
+}
+
+type Props = {
+  image: Image
+  onToggle: (image: Image) => void
+}
+
 const ImageCard = ({
   image: { urls, likes, description },
   onToggle
-}) => {
+}: Props) => {
   const imageInfo = {
     likes: likes,
     modal: urls.regular,
